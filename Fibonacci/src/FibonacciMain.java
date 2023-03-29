60503e6ba48a8ec2a2fe9c1236a6bd978100e066
NaloJacobo <88952388+NaloJacobo@users.noreply.github.com>
2023-03-29 00:31:24 -0600

    Add files via upload

---
+++ Fibonacci/src/FibonacciMain.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author Nalo
+ */
+public class FibonacciMain {
+
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            System.out.print(Fibonacci.fibonacci(i) + " ");
+        }
+    }
+
+}
